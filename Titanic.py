eb8a428e33d8f2d746beecb981f48b3daa699587
dangminh214 <dangminh214@gmail.com>
2024-06-06 11:01:27 +0200

    extract and calculate % survived men, women

--- Titanic.py
+++ Titanic.py
@@ -8,7 +8,6 @@ import pandas as pd
 # data processing, CSV file I/O (e.g. pd.read_csv)
 
 # Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
 
@@ -16,7 +15,25 @@ for dirname, _, filenames in os.walk('kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
-# Any results you write to the current directory are saved as output.
+# Read data
 train_data = pd.read_csv("kaggle/input/titanic/train.csv")
 train_data.head()
 print(train_data)
+
+test_data = pd.read_csv("kaggle/input/titanic/test.csv")
+test_data.head()
+print(test_data)
+
+# % women survived
+women = train_data.loc[train_data.Sex == 'female']["Survived"]
+rate_women = sum(women)/len(women)
+
+print("% of women who survived:", rate_women)
+
+# % men survived
+men = train_data.loc[train_data.Sex == 'male']["Survived"]
+rate_men = sum(men)/len(men)
+
+print("% of men who survived:", rate_men)
+
+
