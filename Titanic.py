eda3d803b43d418a3f485f9b3f1e6a26b904aa83
dangminh214 <dangminh214@gmail.com>
2024-06-06 11:15:31 +0200

    create submission csv

--- Titanic.py
+++ Titanic.py
@@ -5,7 +5,7 @@
 import numpy as np
 # linear algebra
 import pandas as pd
-# data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.ensemble import RandomForestClassifier
 
 # Input data files are available in the "../input/" directory.
 
@@ -18,22 +18,35 @@ for dirname, _, filenames in os.walk('kaggle/input'):
 # Read data
 train_data = pd.read_csv("kaggle/input/titanic/train.csv")
 train_data.head()
-print(train_data)
+print("train data: \n", train_data)
 
 test_data = pd.read_csv("kaggle/input/titanic/test.csv")
 test_data.head()
-print(test_data)
+print("test data: \n", test_data)
 
 # % women survived
 women = train_data.loc[train_data.Sex == 'female']["Survived"]
 rate_women = sum(women)/len(women)
 
-print("% of women who survived:", rate_women)
+print("% of survived women:", rate_women)
 
 # % men survived
 men = train_data.loc[train_data.Sex == 'male']["Survived"]
 rate_men = sum(men)/len(men)
 
-print("% of men who survived:", rate_men)
+print("% of survived men:", rate_men)
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('submission.csv', index=False)
 
 
